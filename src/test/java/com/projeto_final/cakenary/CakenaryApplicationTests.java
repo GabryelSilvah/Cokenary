3dac0db064f0a3c7dab962cb6c8dd95c70e429e8
Gabriel da Silva Rosa <Gabriel46566756@edu.df.senac.br>
2025-03-19 10:18:59 -0300

    Primeiro commit

---
+++ src/test/java/com/projeto_final/cakenary/CakenaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto_final.cakenary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CakenaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
